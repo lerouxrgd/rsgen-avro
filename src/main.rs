e463247c51d1d66a4aca076e7a563f2b99c6cfbf
Romain Leroux <romain@leroux.dev>
2022-11-07 23:26:53 +0100

    Use clap instead of docopt for CLI

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,44 @@
 use std::error::Error;
 use std::fs::OpenOptions;
 use std::io::{prelude::*, stdout};
+use std::path::PathBuf;
 use std::process::{self, Command};
 
-use docopt::Docopt;
+use clap::Parser;
 use rsgen_avro::{Generator, Source};
-use serde::Deserialize;
 
-const VERSION: &str = env!("CARGO_PKG_VERSION");
-
-const USAGE: &str = "
-Usage:
-  rsgen-avro [options] <schema-files-glob-pattern> <output-file>
-  rsgen-avro (-h | --help)
-  rsgen-avro (-V | --version)
-
-Options:
-  --fmt              Run rustfmt on the resulting <output-file>
-  --nullable         Replace null fields with their default value when deserializing.
-  --precision=P      Precision for f32/f64 default values that aren't round numbers [default: 3].
-  --union-deser      Custom deserialization for apache-avro multi-valued union types.
-  --derive-builders  Derive builders for generated record structs.
-  --derive-schemas   Derive AvroSchema for generated record structs.
-  -V, --version      Show version.
-  -h, --help         Show this screen.
-";
-
-#[derive(Debug, Deserialize)]
-struct CmdArgs {
-    arg_schema_files_glob_pattern: String,
-    arg_output_file: String,
-    flag_fmt: bool,
-    flag_nullable: bool,
-    flag_precision: Option<usize>,
-    flag_union_deser: bool,
-    flag_derive_builders: bool,
-    flag_derive_schemas: bool,
-    flag_version: bool,
+/// Generate Rust types from Avro schemas
+#[derive(Debug, Parser)]
+#[command(version)]
+struct Args {
+    /// Glob pattern to select Avro schema files
+    pub glob_pattern: String,
+    /// The file where Rust types will be written, '-' for stdout
+    pub output_file: PathBuf,
+    /// Run rustfmt on the resulting <output-file>
+    #[clap(long)]
+    pub fmt: bool,
+    /// Replace null fields with their default value when deserializing
+    #[clap(long)]
+    pub nullable: bool,
+    /// Precision for f32/f64 default values that aren't round numbers
+    #[clap(long, value_name = "P", default_value_t = 3)]
+    pub precision: usize,
+    /// Custom deserialization for apache-avro multi-valued union types
+    #[clap(long)]
+    pub union_deser: bool,
+    /// Derive builders for generated record structs
+    #[clap(long)]
+    pub derive_builders: bool,
+    /// Derive AvroSchema for generated record structs
+    #[clap(long)]
+    pub derive_schemas: bool,
 }
 
 fn run() -> Result<(), Box<dyn Error>> {
-    let args: CmdArgs = Docopt::new(USAGE).and_then(|d| d.deserialize())?;
+    let args = Args::parse();
 
-    if args.flag_version {
-        println!("{}", VERSION);
-        return Ok(());
-    }
-
-    let mut out: Box<dyn Write> = if &args.arg_output_file == "-" {
+    let mut out: Box<dyn Write> = if args.output_file == PathBuf::from("-") {
         Box::new(stdout())
     } else {
         Box::new(
@@ -55,26 +46,24 @@ fn run() -> Result<(), Box<dyn Error>> {
                 .write(true)
                 .create(true)
                 .truncate(true)
-                .open(&args.arg_output_file)?,
+                .open(&args.output_file)?,
         )
     };
 
-    let source = Source::GlobPattern(&args.arg_schema_files_glob_pattern);
+    let source = Source::GlobPattern(&args.glob_pattern);
 
     let g = Generator::builder()
-        .precision(args.flag_precision.unwrap())
-        .nullable(args.flag_nullable)
-        .use_avro_rs_unions(args.flag_union_deser)
-        .derive_builders(args.flag_derive_builders)
-        .derive_schemas(args.flag_derive_schemas)
+        .precision(args.precision)
+        .nullable(args.nullable)
+        .use_avro_rs_unions(args.union_deser)
+        .derive_builders(args.derive_builders)
+        .derive_schemas(args.derive_schemas)
         .build()?;
 
     g.gen(&source, &mut out)?;
 
-    if args.flag_fmt && &args.arg_output_file != "-" {
-        Command::new("rustfmt")
-            .arg(&args.arg_output_file)
-            .status()?;
+    if args.fmt && args.output_file != PathBuf::from("-") {
+        Command::new("rustfmt").arg(&args.output_file).status()?;
     }
 
     Ok(())
