401779bc4395a4c25a58500e6a93707a52f9e07b
kriskras99 <admin@kriskras99.nl>
2025-07-07 11:31:31 +0000

    feat: Allow to override (part of) the generated code for a field (no CLI support)
    The primary use case for me is using more specific types directly instead of having
    to create adapter structs, for example having `IpAddr` as a field instead of `Vec<u8>`.
    
    I don't have a direct usecase for changing the docstring, but I imagine it can be useful
    when you don't control the schema yourself.

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,9 @@ mod generator;
 mod templates;
 
 pub use crate::error::{Error, Result};
-pub use crate::generator::{Generator, GeneratorBuilder, ImplementAvroSchema, Source};
+pub use crate::generator::{
+    FieldOverride, Generator, GeneratorBuilder, ImplementAvroSchema, Source,
+};
 
 pub use apache_avro;
 pub use apache_avro::Schema;
--- tests/schemas/mod.rs
+++ tests/schemas/mod.rs
@@ -33,7 +33,49 @@ pub mod record_multiline_doc;
 pub mod recursive;
 pub mod simple;
 pub mod simple_with_builders;
+pub mod simple_with_override;
 pub mod simple_with_schemas;
 pub mod simple_with_schemas_impl;
 pub mod nested_with_schemas_impl;
 pub mod nested_with_float;
+
+/// For testing field overrides in simple_with_override
+mod ip_addr_serde {
+    use std::{fmt::Formatter, net::IpAddr};
+    use serde::{de::{Error, Visitor}, Deserializer, Serializer};
+
+    pub fn serialize<S>(value: &IpAddr, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+        match value {
+            IpAddr::V4(ipv4_addr) => serializer.serialize_bytes(&ipv4_addr.octets()),
+            IpAddr::V6(ipv6_addr) => serializer.serialize_bytes(&ipv6_addr.octets()),
+        }
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<IpAddr, D::Error> where D: Deserializer<'de> {
+        struct IpVisitor;
+        impl Visitor<'_> for IpVisitor {
+            type Value = IpAddr;
+
+            fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
+                formatter.write_str("an IPv4 address as bytes")
+            }
+
+            fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E> 
+            where E: Error {
+                match v.len() {
+                    4 => {
+                        let bytes: [u8; 4] = v.try_into().unwrap();
+                        Ok(IpAddr::from(bytes))
+                    }
+                    16 => {
+                        let bytes: [u8; 16] = v.try_into().unwrap();
+                        Ok(IpAddr::from(bytes))
+                    }
+                    _ => Err(Error::custom("Invalid IP address byte length")),
+                }
+            }
+        }
+
+        deserializer.deserialize_bytes(IpVisitor)
+    }
+}
